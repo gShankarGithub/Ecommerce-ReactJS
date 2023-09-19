9f8f8c4ef8767d1f81b726529152fee16aaf896a
gShankarGithub <gokulsshankar.official@gmail.com>
2023-09-19 16:18:37 +0530

    Second Commit categories completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return (
     <div>
-        <Navbar/>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
